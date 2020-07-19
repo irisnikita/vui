a5c0847cd72c097b0d1e25bfaa5056007afe289a
Truong vi <vinlt@antsprogrammatic.com>
2020-07-19 18:33:25 +0700

    Init project

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// Components
+import { Button } from '../components'
+
+export default function index() {
+    return (
+        <div>
+            <Button type='default' />
+        </div>
+    )
+}
